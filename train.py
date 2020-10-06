8379cddbae53f11689f1c7bde6c46de497eef32c
ds-learning-2020 <63844954+ds-learning-2020@users.noreply.github.com>
2020-10-06 17:00:30 +0530

    load and split dataset

--- train.py
+++ train.py
@@ -14,13 +14,13 @@ from azureml.data.dataset_factory import TabularDatasetFactory
 # Data is located at:
 # "https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv"
 
-ds = ### YOUR CODE HERE ###
+# documentation - https://docs.microsoft.com/en-us/python/api/azureml-core/azureml.data.dataset_factory.tabulardatasetfactory
+ds = Dataset.Tabular.from_delimited_files(path="https://automlsamplenotebookdata.blob.core.windows.net/automl-sample-notebook-data/bankmarketing_train.csv")
 
 x, y = clean_data(ds)
 
 # TODO: Split data into train and test sets.
-
-### YOUR CODE HERE ###a
+X_train, X_test , y_train, y_test = train_test_split(x, y, test_size=20, random_state=42)
 
 run = Run.get_context()
 
